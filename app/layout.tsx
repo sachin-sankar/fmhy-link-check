676aa7f772c8a57294b7ef2988c02b0a97f3e355
sachin-sankar <mail.sachinsankar@gmail.com>
2024-11-06 11:34:19 +0530

    chore: Set default colorscheme to dark

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
         <ColorSchemeScript />
       </head>
       <body>
-        <MantineProvider>{children}</MantineProvider>
+        <MantineProvider defaultColorScheme="dark">{children}</MantineProvider>
       </body>
     </html>
   );
